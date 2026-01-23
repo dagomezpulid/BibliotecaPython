deb60be04cd97449665e402898398371d93b4357
dagomezpulid <dgomezpulid@outlook.com>
2026-01-23 14:03:14 -0500

    commit

--- biblioteca/catalogo/admin.py
+++ biblioteca/catalogo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Autor, Libro
 
-# Register your models here.
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
+
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "autor")
+    list_filter = ("autor",)
+    search_fields = ("titulo",)
